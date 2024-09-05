d89850b38f29f480c5c7168d28434aa56096a49e
JeongminJamie <sksrhcalsu@gmail.com>
2024-09-05 17:14:24 +0900

    Setup: install react-router-dom and set up in app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {/* 메인 라우트 안에 자식 라우트 넣어주기 밑은 예시! */}
+      {/* <Route path="/" element={<Main />}>
+        <Route path="/restaurant" element={<></>}></Route>
+      </Route> */}
+    </Routes>
   );
 }
 
