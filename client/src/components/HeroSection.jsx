56398e21edc115b26984b53a074adae6c55e75e7
JeongminJamie <sksrhcalsu@gmail.com>
2024-09-06 15:43:57 +0900

    Design: hero section on main page for background of the main header

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -6,12 +6,12 @@ const HeroSection = () => {
         className="h-screen w-screen object-cover"
         alt="Main"
       />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10">
-        <div className="text-2xl font-bold mb-4">안심식당 예약 시스템</div>
-        <div className="text-lg mb-6">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white z-10">
+        <div className="text-5xl font-bold mb-6">안심식당 예약 시스템</div>
+        <div className="text-2xl font-semibold mb-10">
           안전한 식사, 안심테이블에서 예약하고 편안한 시간을 즐기세요!
         </div>
-        <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600">
+        <button className="px-10 py-3 bg-white text-black text-lg font-semibold hover:bg-amber-100 rounded-full">
           안심식당 찾기
         </button>
       </div>
--- client/src/components/mainHeader.jsx
+++ client/src/components/mainHeader.jsx
@@ -5,13 +5,13 @@ const MainHeader = () => {
     <header className="absolute top-4 left-1/2 transform -translate-x-1/2 z-10 w-9/12 px-3 py-3 bg-white flex flex-row justify-between items-center rounded-full text-center font-medium opacity-85 text-lg">
       <div className="w-2/12 font-bold">안심테이블</div>
       <div className="flex flex-row justify-around w-3/12 font-semibold">
-        <div>식당찾기</div>
-        <div>소개</div>
-        <div>내정보</div>
-        {/* <div>회원가입</div> */}
+        <div className="hover:cursor-pointer">식당찾기</div>
+        <div className="hover:cursor-pointer">소개</div>
+        <div className="hover:cursor-pointer">내정보</div>
+        <div className="hover:cursor-pointer">회원가입</div>
       </div>
-      <div className="w-28 bg-yellow-200 rounded-full py-3 font-bold">
-        로그아웃
+      <div className="w-28 bg-amber-200 rounded-full py-3 font-bold hover:cursor-pointer">
+        로그인
       </div>
     </header>
   );
