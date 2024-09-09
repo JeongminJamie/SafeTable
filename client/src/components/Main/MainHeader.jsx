7c5b25d016bd1d20aefef367a356b09155f17d09
JeongminJamie <sksrhcalsu@gmail.com>
2024-09-10 00:05:10 +0900

    Design: main header elements' font size changes when hovered

--- client/src/components/Main/MainHeader.jsx
+++ client/src/components/Main/MainHeader.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const MainHeader = () => {
   return (
-    <header className="fixed top-4 left-1/2 transform -translate-x-1/2 z-10 w-9/12 px-3 py-3 bg-white flex flex-row justify-between items-center rounded-full text-center font-medium opacity-85 text-lg">
+    <header className="fixed top-4 left-1/2 transform -translate-x-1/2 z-10 w-10/12 px-3 py-3 bg-white flex flex-row justify-between items-center rounded-full text-center font-medium opacity-85 text-lg">
       <div className="w-2/12 font-bold">안심테이블</div>
       <div className="flex flex-row justify-around w-3/12 font-semibold">
-        <div className="hover:cursor-pointer">식당찾기</div>
-        <div className="hover:cursor-pointer">소개</div>
-        <div className="hover:cursor-pointer">내정보</div>
-        <div className="hover:cursor-pointer">회원가입</div>
+        <div className="hover:cursor-pointer hover:text-xl">식당찾기</div>
+        <div className="hover:cursor-pointer hover:text-xl">소개</div>
+        <div className="hover:cursor-pointer hover:text-xl">내정보</div>
+        <div className="hover:cursor-pointer hover:text-xl">회원가입</div>
       </div>
       <div className="w-28 bg-amber-200 rounded-full py-3 font-bold hover:cursor-pointer">
         로그인
