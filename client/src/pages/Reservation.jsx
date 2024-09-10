ecdb5cad26b790f08f2c47ee4ca5b7581d7b89bd
JeongminJamie <sksrhcalsu@gmail.com>
2024-09-10 21:18:54 +0900

    Design: put header and footer on every page on App.js file

--- client/src/pages/Reservation.jsx
+++ client/src/pages/Reservation.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Header from "../components/Header";
 
 const Reservation = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Reservation;
