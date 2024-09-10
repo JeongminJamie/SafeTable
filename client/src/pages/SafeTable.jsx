bf1f97971eccaf1e5e8b4cff9135dd17f997060d
gongjuyeonhee <yyeonhee912@naver.com>
2024-09-10 21:45:38 +0900

    Design: add design safetable card

--- client/src/pages/SafeTable.jsx
+++ client/src/pages/SafeTable.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import SafeHeader from "../components/SafeTable/SafeHeader";
 import SafeMain from "../components/SafeTable/SafeMain";
 
 const SafeTable = () => {
   return (
     <>
-      <SafeHeader />
       <SafeMain />
     </>
   );
