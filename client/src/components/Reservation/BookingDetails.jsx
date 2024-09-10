4854563bb7902237e056bb2e872c7c90b9113f78
JeongminJamie <sksrhcalsu@gmail.com>
2024-09-10 22:20:09 +0900

    Setup: install react datepicker for reservation page

--- client/src/components/Reservation/BookingDetails.jsx
+++ client/src/components/Reservation/BookingDetails.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { PARTY_SIZE, TIME } from "../../constants/reservation";
 
 const BookingDetails = () => {
+  const [date, setDate] = useState(new Date());
+
   return (
     <div className="flex flex-row">
       <div className="flex flex-col">
@@ -14,6 +18,7 @@ const BookingDetails = () => {
       </div>
       <div className="flex flex-col">
         <p>날짜</p>
+        <DatePicker selected={date} />
       </div>
       <div className="flex flex-col">
         <p>시간</p>
