951fd6414eb817616ca2c723934c93469397d719
JeongminJamie <sksrhcalsu@gmail.com>
2024-09-11 16:38:31 +0900

    Install: react-select library for dropdown on reservation page

--- client/src/components/Reservation/BookingDetails.jsx
+++ client/src/components/Reservation/BookingDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Select from "react-select";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { PARTY_SIZE, TIME } from "../../constants/reservation";
@@ -7,24 +8,28 @@ const BookingDetails = () => {
   const [date, setDate] = useState(new Date());
 
   return (
-    <div className="flex flex-row">
-      <div className="flex flex-col">
-        <p>인원</p>
-        <select>
+    <div className="flex flex-row w-full items-center justify-around px-10 gap-10">
+      <div className="flex flex-col w-4/12 gap-2">
+        <p className="text-start font-medium">인원</p>
+        <select className="h-8 text-sm border border-gray-300 rounded-md p-1">
           {PARTY_SIZE.map((party) => (
-            <option>{party}</option>
+            <option key={party}>{party}</option>
           ))}
         </select>
       </div>
-      <div className="flex flex-col">
-        <p>날짜</p>
-        <DatePicker selected={date} />
+      <div className="flex flex-col w-4/12 gap-2">
+        <p className="text-start font-medium">날짜</p>
+        <DatePicker
+          selected={date}
+          onChange={(date) => setDate(date)}
+          className="w-full h-8 text-sm border border-gray-300 rounded-md p-1"
+        />
       </div>
-      <div className="flex flex-col">
-        <p>시간</p>
-        <select>
+      <div className="flex flex-col w-4/12 gap-2">
+        <p className="text-start font-medium">시간</p>
+        <select className="h-8 text-sm border border-gray-300 rounded-md p-1">
           {TIME.map((time) => (
-            <option>{time}</option>
+            <option key={time}>{time}</option>
           ))}
         </select>
       </div>
--- client/src/pages/Reservation.jsx
+++ client/src/pages/Reservation.jsx
@@ -4,7 +4,7 @@ import TimeSlot from "../components/Reservation/TimeSlot";
 
 const Reservation = () => {
   return (
-    <div className="h-screen flex flex-col items-center text-center">
+    <div className="w-4/6 h-screen m-auto flex flex-col items-center text-center gap-10">
       <div>
         <p>MAKE A RESERVATION</p>
         <p>테이블 예약을 위해 인원 수, 날짜, 시간대를 선택해 주세요.</p>
