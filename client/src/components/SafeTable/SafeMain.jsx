ca0fd55140189709ce2fa99a617c4f32f80c6b8a
gongjuyeonhee <yyeonhee912@naver.com>
2024-09-12 17:36:13 +0900

    Design: Update TableCard design for improved layout and styling

--- client/src/components/SafeTable/SafeMain.jsx
+++ client/src/components/SafeTable/SafeMain.jsx
@@ -6,9 +6,9 @@ const SafeMain = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="p-10">
-      {/* 식당 카드 - 최대 5열 */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 p-10">
+    <div className="px-10">
+      {/* 식당 카드 - 최대 4열 */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-x-5 p-5  gap-y-10">
         <div onClick={() => navigate("/safetabledetail")}>
           <TableCard />
         </div>
