94cdae6945d910f78be29d0d1b9a90ce8cef8b63
JeongminJamie <sksrhcalsu@gmail.com>
2024-09-25 16:24:45 +0900

    Install: install react query

--- client/src/components/SafeTable/SearchBox.jsx
+++ client/src/components/SafeTable/SearchBox.jsx
@@ -1,91 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import { InputAdornment } from "@mui/material";
+import axios from "axios";
 
 const SearchBox = () => {
-  // Top 5 Nigerian songs on Apple Music
-  const top5Songs = [
-    { label: "Organize" },
-    { label: "Joha" },
-    { label: "Terminator" },
-    { label: "Dull" },
-    { label: "Nzaza" },
-  ];
+  const [inputValue, setInputValue] = useState("");
+  const [options, setOptions] = useState([]);
 
-  // return (
-  //   <div className="flex justify-center items-center w-4/12 h-12 border border-gray-500 rounded-full mx-auto my-14 p-4">
-  //     <input
-  //       className="w-full focus:outline-none
-  //       placeholder:font-medium placeholder:text-center"
-  //       placeholder="지역 검색"
-  //     ></input>
-  //     <img
-  //       src="/assets/safeTable/search.png"
-  //       alt="search"
-  //       className="w-7 h-7 hover:cursor-pointer"
-  //     />
-  //   </div>
-  // );
+  // input 결과값에 따른 행정구역 open api로 패치 요청
+  const fetchRegionsByinput = async () => {
+    if (inputValue) {
+      try {
+        const response = await axios.get(`YOUR_API_URL?query=${inputValue}`);
+        console.log("행정구역 패치 결과 확인", response);
+        setOptions(response.data);
+      } catch (error) {
+        console.error(
+          "검색어에 따른 행정구역 명을 패치하는 도중 오류 발생",
+          error
+        );
+      }
+    }
+  };
+
+  // 검색어가 변할 때마다, 타이핑을 끝낸 후(300ms 예상)에 지역명을 옵션으로 보여주기
+  useEffect(() => {
+    const debounceTimeout = setTimeout(() => {
+      fetchRegionsByinput();
+    }, 300);
+
+    return () => {
+      clearTimeout(debounceTimeout);
+    };
+  }, [inputValue]);
+
   return (
     <Autocomplete
-      disablePortal
-      id="combo-box-demo"
-      options={top5Songs}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="Songs" />}
+      freeSolo
+      value={inputValue}
+      options={options}
+      onInputChange={(event, inputWord) => {
+        setInputValue(inputWord);
+      }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          placeholder="지역 검색"
+          variant="standard"
+          slotProps={{
+            input: {
+              disableUnderline: true,
+              endAdornment: (
+                <InputAdornment position="end">
+                  <img
+                    src="/assets/safeTable/search.png"
+                    alt="search"
+                    className="w-7 h-7 hover:cursor-pointer"
+                  />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      )}
+      renderOption={(props, option) => <li {...props}>{option}</li>}
+      className="flex justify-center items-center w-4/12 h-12 border border-gray-500 rounded-full mx-auto my-14 p-4"
     />
   );
 };
 
 export default SearchBox;
 
-// import React, { useState, useEffect } from 'react';
-// import Autocomplete from '@mui/material/Autocomplete';
-// import TextField from '@mui/material/TextField';
-// import axios from 'axios';
-
-// const LocationSearch = () => {
-//   const [inputValue, setInputValue] = useState('');
-//   const [options, setOptions] = useState([]);
-
-//   useEffect(() => {
-//     const fetchLocations = async () => {
-//       if (inputValue) {
-//         try {
-//           const response = await axios.get(`YOUR_API_URL?query=${inputValue}`);
-//           // response.data에서 지역명 추출
-//           const locationNames = response.data.map(location => location.name); // 데이터 구조에 맞게 조정
-//           setOptions(locationNames);
-//         } catch (error) {
-//           console.error('Error fetching locations:', error);
-//         }
-//       } else {
-//         setOptions([]);
-//       }
-//     };
-
-//     const debounceTimeout = setTimeout(() => {
-//       fetchLocations();
-//     }, 300); // 300ms 지연 후 API 호출
-
 //     return () => clearTimeout(debounceTimeout);
 //   }, [inputValue]);
-
-//   return (
-//     <Autocomplete
-//       freeSolo
-//       options={options}
-//       onInputChange={(event, newInputValue) => {
-//         setInputValue(newInputValue);
-//       }}
-//       renderInput={(params) => (
-//         <TextField {...params} label="Search for a location" variant="outlined" />
-//       )}
-//       renderOption={(props, option) => (
-//         <li {...props}>{option}</li>
-//       )}
-//     />
-//   );
-// };
-
-// export default LocationSearch;
