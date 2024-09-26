85b298b7e5eb03965f3747c411023afc722334f7
JeongminJamie <sksrhcalsu@gmail.com>
2024-09-26 17:45:36 +0900

    Feat: fetch regions of korea using data open api and work autocomplete with the region data

--- client/src/components/SafeTable/SearchBox.jsx
+++ client/src/components/SafeTable/SearchBox.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import { InputAdornment } from "@mui/material";
@@ -6,42 +7,60 @@ import axios from "axios";
 
 const SearchBox = () => {
   const [inputValue, setInputValue] = useState("");
-  const [options, setOptions] = useState([]);
+  const [debouncedInputValue, setDebouncedInputValue] = useState("");
 
-  // input 결과값에 따른 행정구역 open api로 패치 요청
-  const fetchRegionsByinput = async () => {
-    if (inputValue) {
-      try {
-        const response = await axios.get(`YOUR_API_URL?query=${inputValue}`);
-        console.log("행정구역 패치 결과 확인", response);
-        setOptions(response.data);
-      } catch (error) {
-        console.error(
-          "검색어에 따른 행정구역 명을 패치하는 도중 오류 발생",
-          error
-        );
-      }
-    }
+  // To-do: 엔터키를 누르거나, 검색 아이콘을 눌렀을 때 입력값에 맞는 지역 안심식당 정보 패치하기!!!
+
+  // 입력값에 따른 시군구 open api로 데이터 패치 요청
+  const fetchRegionsByInput = async (inputValue) => {
+    const encodedInputValue = encodeURIComponent(inputValue);
+
+    const response = await axios.get(
+      `/2ddata/adsigg/data?apiKey=${process.env.REACT_APP_LOCATION_API_KEY}&domain=${process.env.REACT_APP_LOCATION_DOMAIN}&filter=full_nm:like:${encodedInputValue}&output=json&pageIndex=1&pageUnit=10`
+    );
+
+    const featuresOfRegions = response.data.featureCollection?.features || [];
+    const regionNames = featuresOfRegions.map(
+      (feature) => feature.properties.full_nm
+    );
+
+    return regionNames;
   };
 
-  // 검색어가 변할 때마다, 타이핑을 끝낸 후(300ms 예상)에 지역명을 옵션으로 보여주기
+  // 검색어가 변할 때마다, 타이핑을 끝낸 후(300ms 예상) debouncedInputValue에 저장해줌
   useEffect(() => {
     const debounceTimeout = setTimeout(() => {
-      fetchRegionsByinput();
-    }, 300);
+      setDebouncedInputValue(inputValue);
+    }, 200);
 
     return () => {
       clearTimeout(debounceTimeout);
     };
   }, [inputValue]);
 
+  // useQuery로 데이터를 가져옴
+  const {
+    data: options = [],
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["fetchRegions", debouncedInputValue],
+    queryFn: () => fetchRegionsByInput(debouncedInputValue),
+    enabled: !!debouncedInputValue,
+    refetchOnWindowFocus: false,
+  });
+
   return (
     <Autocomplete
       freeSolo
-      value={inputValue}
       options={options}
-      onInputChange={(event, inputWord) => {
-        setInputValue(inputWord);
+      inputValue={inputValue}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      onChange={(event, newValue) => {
+        setInputValue(newValue || "");
       }}
       renderInput={(params) => (
         <TextField
@@ -50,9 +69,13 @@ const SearchBox = () => {
           variant="standard"
           slotProps={{
             input: {
+              ...params.InputProps,
               disableUnderline: true,
               endAdornment: (
-                <InputAdornment position="end">
+                <InputAdornment
+                  position="end"
+                  sx={{ position: "absolute", marginLeft: "95%" }}
+                >
                   <img
                     src="/assets/safeTable/search.png"
                     alt="search"
@@ -64,13 +87,9 @@ const SearchBox = () => {
           }}
         />
       )}
-      renderOption={(props, option) => <li {...props}>{option}</li>}
       className="flex justify-center items-center w-4/12 h-12 border border-gray-500 rounded-full mx-auto my-14 p-4"
     />
   );
 };
 
 export default SearchBox;
-
-//     return () => clearTimeout(debounceTimeout);
-//   }, [inputValue]);
