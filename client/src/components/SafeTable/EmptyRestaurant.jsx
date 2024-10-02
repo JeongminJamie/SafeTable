43c20db6224a10ffab8a3fa6af964f83c9f7312b
JeongminJamie <sksrhcalsu@gmail.com>
2024-10-02 17:09:34 +0900

    Design: put emptyRestaurant in SafeMain file and comment out at the moment

--- client/src/components/SafeTable/EmptyRestaurant.jsx
+++ client/src/components/SafeTable/EmptyRestaurant.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const EmptyRestaurant = () => {
   //store에서 inputValue 나중에 가져오기
+
   return (
-    <div className="flex flex-col items-center h-screen w-full gap-5">
+    <div className="flex flex-col items-center h-screen w-full gap-5 mt-10">
       <img src="/assets/empty.png" alt="empty" className="w-20 h-20" />
-      <div className="font-base text-lg">
-        inputValue에 대한 결과가 없습니다.
+      <div>
+        <span className="font-medium">"inputValue"</span>에 대한 안심식당 정보가
+        없습니다.
       </div>
     </div>
   );
--- client/src/components/SafeTable/SafeMain.jsx
+++ client/src/components/SafeTable/SafeMain.jsx
@@ -10,7 +10,6 @@ const SafeMain = () => {
     <div className="px-10">
       {/* 식당 카드 - 최대 4열 */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-x-5 p-5  gap-y-10">
-        {/* <TableCard />
         <TableCard />
         <TableCard />
         <TableCard />
@@ -19,8 +18,10 @@ const SafeMain = () => {
         <TableCard />
         <TableCard />
         <TableCard />
-        <TableCard /> */}
+        <TableCard />
+        <TableCard />
       </div>
+      {/* 입력값에 따른 안심식당 정보가 없을 때 띄울 컴포넌트 */}
       <EmptyRestaurant />
     </div>
   );
