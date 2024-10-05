b6c24b71ab1e734378f37dfef269ec850a73583c
JeongminJamie <sksrhcalsu@gmail.com>
2024-10-05 22:43:58 +0900

    Design: save restaurant image for about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import AboutImage from "../components/About/AboutImage";
+import Introduction from "../components/About/Introduction";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="h-screen">
+      <AboutImage />
+      <Introduction />
+    </div>
+  );
 };
 
 export default About;
