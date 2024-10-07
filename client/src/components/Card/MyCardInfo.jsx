39cf6ae1658ae5e36a680bed90126a151fa53c7e
JeongminJamie <sksrhcalsu@gmail.com>
2024-10-07 17:02:21 +0900

    Design: add NoCard and IncorrectCardModal on Mypage

---
+++ client/src/components/Card/MyCardInfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MyCardInfo = () => {
+  return (
+    <div>
+      {/* To-do: 내 카드 데이터가 없으면 NoCard 컴포넌트 띄우기!! */}
+      my card
+    </div>
+  );
+};
+
+export default MyCardInfo;
---
+++ client/src/components/Card/NoCard.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import AddCard from "./AddCard";
+
+const NoCard = () => {
+  const [isRegisterClicked, setIsRegisterClicked] = useState(false);
+  return (
+    <>
+      {isRegisterClicked ? (
+        <div className="px-10 py-0 mx-5 mb-5 flex flex-col justify-center ">
+          <AddCard />
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center my-24 mx-20 gap-5">
+          <img src="/assets/empty.png" alt="empty" className="w-24 h-24" />
+          <div className="font-medium text-lg">등록된 카드가 없습니다</div>
+          <button
+            className="rounded font-medium w-2/12 h-12 bg-amber-500 text-white m-auto"
+            onClick={() => setIsRegisterClicked(true)}
+          >
+            카드 등록
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default NoCard;
