80e38b4a55a5ceaafdd469772bf6c640394dcc7e
JeongminJamie <sksrhcalsu@gmail.com>
2024-10-15 19:23:46 +0900

    Feat: make infinite scroll for entire restaurants and creat a custom hook of observer with throttle

---
+++ client/src/hooks/useObserverWithThrottle.js
@@ -0,0 +1,38 @@
+import { useEffect, useRef } from "react";
+
+const useObserberWithThrottle = ({
+  fetchNextPage,
+  hasNextPage,
+  isFetchingNextPage,
+}) => {
+  const loadMoreRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+          fetchNextPage();
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5,
+      }
+    );
+
+    if (loadMoreRef.current) {
+      observer.observe(loadMoreRef.current);
+    }
+
+    return () => {
+      if (loadMoreRef.current) {
+        observer.unobserve(loadMoreRef.current);
+      }
+    };
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
+  return loadMoreRef;
+};
+
+export default useObserberWithThrottle;
--- client/src/pages/SafeTable.jsx
+++ client/src/pages/SafeTable.jsx
@@ -1,39 +1,53 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect } from "react";
 import SafeMain from "../components/SafeTable/SafeMain";
 import SearchBox from "../components/SafeTable/SearchBox";
 import axios from "axios";
 import fisherYatesShuffle from "../utils/fisherYatesShuffle";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import useRestaurantStore from "../store/useRestaurantStore";
+import useObserberWithThrottle from "../hooks/useObserverWithThrottle";
 
 const SafeTable = () => {
   const { setFetchedRestaurants } = useRestaurantStore();
 
-  const fetchEntireRestaurants = async () => {
+  const fetchEntireRestaurants = async (pageParam) => {
     try {
       const response = await axios.get(
-        `${process.env.REACT_APP_SERVER_PORT_URL}/api/restaurants/openapi/${process.env.REACT_APP_RESTAURANT_API_KEY}/json/Grid_20200713000000000605_1/1/100?RELAX_SI_NM=서울특별시`
+        `${process.env.REACT_APP_SERVER_PORT_URL}/api/restaurants/openapi/${process.env.REACT_APP_RESTAURANT_API_KEY}/json/Grid_20200713000000000605_1/${pageParam}/100?RELAX_SI_NM=서울특별시`
       );
 
       const data = response.data.Grid_20200713000000000605_1.row;
       const shuffledData = await fisherYatesShuffle(data);
 
-      return shuffledData;
+      return { data: shuffledData, nextCursor: pageParam + 1 };
     } catch (error) {
       console.error("안심식당 전체 정보 조회 중 에러 발생", error);
     }
   };
 
   // To-do: isLoading이 1초 이상 넘어가면 스켈레톤 화면 띄워주기
-  const { data, isLoading } = useQuery({
-    queryKey: ["fetchEntireRestaurants"],
-    queryFn: () => fetchEntireRestaurants(),
-    staleTime: 60 * 1000,
+  // 전체 안심식당 조회 및 무한 스크롤링
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
+    useInfiniteQuery({
+      queryKey: ["fetchEntireRestaurants"],
+      queryFn: ({ pageParam }) => fetchEntireRestaurants(pageParam),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, pages) => {
+        return lastPage?.nextCursor ? lastPage.nextCursor : undefined;
+      },
+    });
+
+  // 무한 스크롤링을 위한 observer 커스텀 훅 호출
+  const loadMoreRef = useObserberWithThrottle({
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
   });
 
   useEffect(() => {
     if (data) {
-      setFetchedRestaurants(data);
+      const allFetchedRestaurants = data.pages.flatMap((page) => page.data);
+      setFetchedRestaurants(allFetchedRestaurants);
     }
   }, [data]);
 
@@ -41,6 +55,14 @@ const SafeTable = () => {
     <>
       <SearchBox />
       <SafeMain />
+      <div ref={loadMoreRef}></div>
+      {isFetchingNextPage && (
+        <img
+          src="/assets/loading-animation.gif"
+          alt="loading"
+          className="m-auto w-32 h-32 p-10"
+        />
+      )}
     </>
   );
 };
