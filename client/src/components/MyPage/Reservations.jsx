054ea4d03bb0b886683fd87eea9e30cbccec0610
JeongminJamie <sksrhcalsu@gmail.com>
2024-10-29 16:20:29 +0900

    Feat: add Loading when first loading in myReservations

--- client/src/components/MyPage/Reservations.jsx
+++ client/src/components/MyPage/Reservations.jsx
@@ -6,6 +6,7 @@ import {
   formatTimeToKoean,
   parseStringToDate,
 } from "../../utils/dateAndTime";
+import Loading from "../Loading";
 
 export const Reservations = () => {
   const [currentReservations, setCurrentReservations] = useState([]);
@@ -64,103 +65,113 @@ export const Reservations = () => {
   };
 
   return (
-    <div>
-      {/* 현재 예약 */}
-      <h2 className="text-xl font-semibold mb-4">Current Reservations</h2>
-      <ul>
-        {currentReservations.length > 0 ? (
-          currentReservations.map((reservation) => (
-            <li
-              key={reservation._id}
-              className="mb-4 border p-4 rounded shadow flex justify-between items-center"
-            >
-              <div>
-                <p>
-                  <strong>Restaurant:</strong> {reservation.name}
-                </p>
-                <p>
-                  <strong>Location:</strong> {reservation.address}
-                </p>
-                <div className="flex gap-5">
-                  <p className="flex items-center">
-                    <img
-                      src="./assets/person.svg"
-                      className="w-4 h-4 mr-1"
-                      alt="Person Icon"
-                    />
-                    {reservation.party_size}
-                  </p>
-                  <p className="flex items-center gap-2">
-                    <img
-                      src="./assets/date.svg"
-                      className="w-4 h-4"
-                      alt="Date Icon"
-                    />
-                    {formattedDate(reservation)} / {formattedTime(reservation)}
-                  </p>
-                </div>
-              </div>
-              <button
-                className="mt-2 p-2 bg-red-500 text-white rounded"
-                onClick={() => handleDelete(reservation._id)}
-              >
-                예약 취소
-              </button>
-            </li>
-          ))
-        ) : (
-          <p className="text-center font-bold my-10">
-            현재 예약된 식당이 없습니다.
-          </p>
-        )}
-      </ul>
-      {/* 구분선 */}
-      <div className="border-b border-gray-300 mt-14"></div>
+    <>
+      {isLoading ? (
+        <Loading width="w-32" height="h-32" padding="p-10 mt-24 mb-24" />
+      ) : (
+        <div>
+          {/* 현재 예약 */}
+          <h2 className="text-xl font-semibold mb-4">Current Reservations</h2>
+          <ul>
+            {currentReservations.length > 0 ? (
+              currentReservations.map((reservation) => (
+                <li
+                  key={reservation._id}
+                  className="mb-4 border p-4 rounded shadow flex justify-between items-center"
+                >
+                  <div>
+                    <p>
+                      <strong>Restaurant:</strong> {reservation.name}
+                    </p>
+                    <p>
+                      <strong>Location:</strong> {reservation.address}
+                    </p>
+                    <div className="flex gap-5">
+                      <p className="flex items-center">
+                        <img
+                          src="./assets/person.svg"
+                          className="w-4 h-4 mr-1"
+                          alt="Person Icon"
+                        />
+                        {reservation.party_size}
+                      </p>
+                      <p className="flex items-center gap-2">
+                        <img
+                          src="./assets/date.svg"
+                          className="w-4 h-4"
+                          alt="Date Icon"
+                        />
+                        {formattedDate(reservation)} /{" "}
+                        {formattedTime(reservation)}
+                      </p>
+                    </div>
+                  </div>
+                  <button
+                    className="mt-2 p-2 bg-red-500 text-white rounded"
+                    onClick={() => handleDelete(reservation._id)}
+                  >
+                    예약 취소
+                  </button>
+                </li>
+              ))
+            ) : (
+              <p className="text-center font-bold my-10">
+                현재 예약된 식당이 없습니다.
+              </p>
+            )}
+          </ul>
+          {/* 구분선 */}
+          <div className="border-b border-gray-300 mt-14"></div>
 
-      {/* 과거 예약 */}
-      <h2 className="text-xl font-semibold mb-6 mt-8">Past Reservations</h2>
-      <ul>
-        {pastReservations.length > 0 ? (
-          pastReservations.map((reservation) => (
-            <li
-              key={reservation._id}
-              className="mb-4 border p-4 rounded shadow flex justify-between items-center"
-            >
-              <div>
-                <p>
-                  <strong>Restaurant:</strong> {reservation.name}
-                </p>
-                <p>
-                  <strong>Location:</strong> {reservation.address}
-                </p>
-                <div className="flex gap-5">
-                  <p className="flex items-center">
-                    <img
-                      src="./assets/person.svg"
-                      className="w-4 h-4 mr-1"
-                      alt="Person Icon"
-                    />
-                    {reservation.party_size}
-                  </p>
-                  <p className="flex items-center gap-2">
-                    <img
-                      src="./assets/date.svg"
-                      className="w-4 h-4"
-                      alt="Date Icon"
-                    />
-                    {formattedDate(reservation)} / {formattedTime(reservation)}
-                  </p>
-                </div>
-                <p>
-                  <strong className="text-green-500">예약완료</strong>
-                </p>
-              </div>
-            </li>
-          ))
-        ) : (
-          <p className="text-center font-bold">과거 예약된 식당이 없습니다.</p>
-        )}
-      </ul>
-    </div>
+          {/* 과거 예약 */}
+          <h2 className="text-xl font-semibold mb-6 mt-8">Past Reservations</h2>
+          <ul>
+            {pastReservations.length > 0 ? (
+              pastReservations.map((reservation) => (
+                <li
+                  key={reservation._id}
+                  className="mb-4 border p-4 rounded shadow flex justify-between items-center"
+                >
+                  <div>
+                    <p>
+                      <strong>Restaurant:</strong> {reservation.name}
+                    </p>
+                    <p>
+                      <strong>Location:</strong> {reservation.address}
+                    </p>
+                    <div className="flex gap-5">
+                      <p className="flex items-center">
+                        <img
+                          src="./assets/person.svg"
+                          className="w-4 h-4 mr-1"
+                          alt="Person Icon"
+                        />
+                        {reservation.party_size}
+                      </p>
+                      <p className="flex items-center gap-2">
+                        <img
+                          src="./assets/date.svg"
+                          className="w-4 h-4"
+                          alt="Date Icon"
+                        />
+                        {formattedDate(reservation)} /{" "}
+                        {formattedTime(reservation)}
+                      </p>
+                    </div>
+                    <p>
+                      <strong className="text-green-500">예약완료</strong>
+                    </p>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <p className="text-center font-bold">
+                과거 예약된 식당이 없습니다.
+              </p>
+            )}
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
