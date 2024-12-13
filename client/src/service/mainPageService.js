a62ecc1c62643eeba5616f2f8c77d1fe4909202f
JeongminJamie <sksrhcalsu@gmail.com>
2024-12-13 15:19:55 +0900

    Fix: generate the start index number in getFourRestaurantsInfo func to get them randomly and refetch func in EachRestaurant for only getting restaurants with photo

--- client/src/service/mainPageService.js
+++ client/src/service/mainPageService.js
@@ -3,8 +3,13 @@ import { api } from "../api/api";
 const restaurantAPIKey = process.env.REACT_APP_RESTAURANT_API_KEY;
 
 export const getFourRestaurantsInfo = async () => {
+  // 랜덤으로 4개의 식당 가져오기 
+  const randomStartIndex = Math.floor(Math.random() * 100);
+
   const response = await api.get(
-    `/api/restaurants/openapi/${restaurantAPIKey}/json/Grid_20200713000000000605_1/0/4?RELAX_SI_NM=서울특별시`
+    `/api/restaurants/openapi/${restaurantAPIKey}/json/Grid_20200713000000000605_1/${randomStartIndex}/${
+      randomStartIndex + 3
+    }?RELAX_SI_NM=서울특별시`
   );
 
   const restaurantData = response.data.Grid_20200713000000000605_1.row;
